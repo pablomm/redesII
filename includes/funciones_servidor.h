17c6ef00f739fab8594a720ad49f3482355ebdb3
dionivillos <dioni37_rubio@hotmail.com>
2017-02-14 18:06:08 +0100

    Borrador básico de las funciones del servidor

---
+++ includes/funciones_servidor.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_SERVIDOR_H
+#define FUNCIONES_SERVIDOR_H
+
+#include "config.h
+
+void liberarEstructuras(void);
+void procesaMensaje(void* pdesc);
+void nuevaConexion(void* userdata);
+
+#endif /* FUNCIONES_SERVIDOR_H */
---
+++ src/funciones_servidor.c
@@ -0,0 +1,11 @@
+#include "../includes/funciones_servidor.h"
+
+void liberarEstructuras(void){
+
+}
+void procesaMensaje(void* pdesc){
+
+}
+void nuevaConexion(void* userdata){
+
+}
