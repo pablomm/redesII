81a7e564d856e0f65d6016b222a5939d92f427bb
pablomm <pablo.mark@hotmail.com>
2017-02-21 02:26:44 +0100

    simple main que lanza el servidor

--- src/servidor.c
+++ src/servidor.c
@@ -5,94 +5,21 @@
 
 
 
-int main(int argc, char * argv[]){
+int main(void){
 
-	int sckt, desc, descraux;
-	int i;
-	fd_set active_fd_set, read_fd_set;
-	pthread_t hiloAux;
-
-	/* Recepcion de argumentos */
-
-
-
-
-	/* deomonizamos / no si flag -h */
+	/* deomonizamos */
 	abrirLog(SERVICIO, LOG_DEBUG);
-	/* daemonizar(SERVICIO, LOG_DEBUG); */
+	/*daemonizar(SERVICIO, LOG_DEBUG);*/
 
-	/* montamos manejadores */
-
+	/* Inicializamos */
 	inicializarServidor();
-	/* Creamos socket TCP */
-	sckt = crearSocketTCP(DEFAULT_PORT, MAX_QUEUE);
-	if(sckt < 0){
-		exit(EXIT_FAILURE);
-	}
 
-	/* Inicializamos conjunto de sockets */
-	FD_ZERO(&active_fd_set);
-  	FD_SET(sckt, &active_fd_set);
+	/* Lanzamos el servidor */
+	lanzarServidor(DEFAULT_PORT);	
 
-	while(1){
-
-		/* Igualamos sockets leidos a los activos */
-		read_fd_set = active_fd_set;
-
-		/* LLamada select bloqueante */
-		if (select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0){
-          	syslog(LOG_ERR, "Error en llamada a select");
-          	break;
-    	}
-
-      	/* Bucle para procesar sockets pendientes */
-      	for (i = 0; i < FD_SETSIZE; ++i){
-
-			/* Solo procesamos sockets marcados */
-		    if (FD_ISSET(i,&read_fd_set)){
-
-				/* Caso conexion nueva */		        
-				if (i == sckt){
-
-						/* down */
-					/* Aceptamos conexion */
-		            if((desc = aceptarConexion(sckt))  < 0){
-		                syslog(LOG_WARNING, "No se ha podido procesar nueva conexion");
-		            }
-
-					/* Creamos estructuras nueva conexion */
-					if(pthread_create(&hiloAux , NULL, nuevaConexion, (void *) &desc)  < 0 ){
-						syslog(LOG_WARNING, "No se ha podido crear hilo con nuevaConexion()");
-
-					}
-					/* Incluimos nuevo descriptor */
-		            FD_SET (desc, &active_fd_set);
-				
-				/* Caso ya aceptado */		       
-				} else {
-		            /* Crea un hilo para procesar el mensaje */
-					/* hay que usar semaforo ?? */
-
-					/* DOwn */
-					descraux = i;
-					
-		         	if (pthread_create(&hiloAux , NULL, procesaMensaje, (void *) &descraux)  < 0) {
-
-		                if(close(i) < 0){
- 							syslog(LOG_WARNING, "No se ha podido cerrar conexion");
-						}
-		                FD_CLR (i, &active_fd_set);
-					}
-
-				}
-			}
-		}	
-	}
-
-		/* liberar lo que sea */
-
+	/* Liberamos estructuras */
 	cerrarServidor();
 
-	exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
 }
 
