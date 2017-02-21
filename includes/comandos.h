e7434c817eeeabd747344e50e2976f87cddf2f89
pablomm <pablo.mark@hotmail.com>
2017-02-21 02:27:34 +0100

    Comandos.c/h creo que seria mejor borrarlos dioni

---
+++ includes/comandos.h
@@ -0,0 +1,11 @@
+#ifndef COMANDOS_H
+#define COMANDOS_H
+
+#include "config.h"
+#include "funciones_servidor.h"
+/*
+status nick(char* comando, pDatosMensaje datos);
+status user(char* comando, pDatosMensaje datos);
+status comandoVacio(char* comando, pDatosMensaje datos);
+*/
+#endif /* COMANDOS_H */
---
+++ src/comandos.c
@@ -0,0 +1,8 @@
+
+#include "../includes/comandos.h"
+
+/*
+status nick(char* comando, pDatosMensaje datos);
+status user(char* comando, pDatosMensaje datos);
+status comandoVacio(char* comando, pDatosMensaje datos);
+*/
