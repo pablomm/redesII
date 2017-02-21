df1ccc0448b2323da521675a25ed7f365f7d5487
pablomm <pablo.mark@hotmail.com>
2017-02-21 02:27:07 +0100

    Comenzando parseo de mensajes

--- includes/funciones_servidor.h
+++ includes/funciones_servidor.h
@@ -1,10 +1,26 @@
 #ifndef FUNCIONES_SERVIDOR_H
 #define FUNCIONES_SERVIDOR_H
 
-#include "config.h
+#include "config.h"
+#include "red_servidor.h"
+
+
+#define N_COMANDOS 100
+
+typedef struct _datosMensaje {
+	int sckfd;
+	char * msg;
+	size_t len;
+} DatosMensaje, *pDatosMensaje;
+
+typedef status (*ArrayComandos)(char*, pDatosMensaje);
+
+ArrayComandos comandos[N_COMANDOS];
 
 void liberarEstructuras(void);
-void procesaMensaje(void* pdesc);
-void nuevaConexion(void* userdata);
+void *manejaMensaje(void* pdesc);
+status nuevaConexion(int desc, struct sockaddr_in address);
+void liberaDatosMensaje(pDatosMensaje datos);
+status crea_comandos();
 
 #endif /* FUNCIONES_SERVIDOR_H */
--- src/funciones_servidor.c
+++ src/funciones_servidor.c
@@ -1,15 +1,59 @@
 #include "../includes/funciones_servidor.h"
 
+
 void liberarEstructuras(void){
 
-}
-void procesaMensaje(void* pdesc){
-
-
-
-
+	printf("Llamada libera estructura\n");
 
 }
-void nuevaConexion(void* userdata){
 
+void *manejaMensaje(void* pdesc){
+
+	pDatosMensaje datos;
+	datos = (pDatosMensaje) pdesc;
+
+	/*
+    syslog(LOG_INFO, "Mensaje: [%s], len=%lu", (char*) datos->msg, strlen(datos->msg));
+    next = IRC_UnPipelineCommands (datos->msg, &comando, strlen(comando));
+    do {
+        procesaMensaje(comando, pdesc);
+        if(comando!=NULL) free(comando);
+        next = IRC_UnPipelineCommands(NULL, &comando, next);
+    }while(next=!NULL)  */
+
+	printf("Mensaje(%d): %s",datos->sckfd, datos->msg);
+	
+	liberaDatosMensaje(datos);
+	addFd(datos->sckfd);
+
+	return NULL;
 }
+status nuevaConexion(int desc, struct sockaddr_in address){
+
+	addFd(desc);
+
+	printf("Llamada nueva conexion\n");
+
+	return 0;
+}
+
+void liberaDatosMensaje(pDatosMensaje datos){
+	free(datos->msg);
+	datos->msg = NULL;
+	free(datos);
+}
+/*
+status crea_comandos() {
+    int i;    
+    for(i=0; i<N_COMANDOS; i++) {
+        comandos[i] = comandoVacio; 
+	}
+
+    comandos[0]=nick;
+    comandos[1]=user; 
+	return 0;   
+}
+
+*/
+
+
